5be1ae006b4bbc29be41887389bba5f4c5008f9d
06needhamt <06needhamt@gmail.com>
2014-02-18 19:28:47 +0000

    Fixes

--- Preprocessor/Preprocessor/Main.cpp
+++ Preprocessor/Preprocessor/Main.cpp
@@ -1,3 +1,4 @@
+#define _SCL_SECURE_NO_WARNINGS 1
 #include<iostream>
 #include<string>
 #include<fstream>
@@ -9,6 +10,8 @@ int main(int argc, char* argv[])
 	if (argc >= 3)
 	{
 		currentdirectory = argv[0];
+		std::cout << argv[1] << std::endl;
+		std::cout << argv[2] << std::endl;
 		inputfile = argv[1];
 		outputfile = argv[2];
 		includepreprocess();
@@ -26,15 +29,21 @@ void includepreprocess()
 {
 	/*const char* cinputfile = inputfile; */
 	std::string line;
+
 	char* cline = new char[line.size() + 1];
 	inputstream.open(inputfile, 'r');
 	outputstream.open(outputfile, 'w');
-	std::copy(line.begin(), line.end(), cline);
-	cline[line.size() + 1] = '\0';
+	//std::copy(line.begin(), line.end(), cline);
+	//cline[line.size() + 1] = '\0';
 	while (getline(inputstream, line))
 	{
+		std::copy(line.begin(), line.end(), cline);
+		std::cout << cline << std::endl;
 		outputstream.write(cline, 'w');
 	}
+	inputstream.close();
+	outputstream.close();
+	system("PAUSE");
 	delete[] cline;
 
 }
