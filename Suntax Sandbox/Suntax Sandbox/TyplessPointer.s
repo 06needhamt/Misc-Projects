8c01d60d599eb54492907d87812061629522665d
06needhamt <06needhamt@gmail.com>
2014-09-18 19:29:21 +0100

    2

---
+++ Suntax Sandbox/Suntax Sandbox/TyplessPointer.s
@@ -0,0 +1,14 @@
+	.file	"TyplessPointer.cpp"
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+	pushl	%ebp
+	movl	%esp, %ebp
+	andl	$-16, %esp
+	call	___main
+	movl	$0, %eax
+	leave
+	ret
+	.ident	"GCC: (tdm-2) 4.8.1"
