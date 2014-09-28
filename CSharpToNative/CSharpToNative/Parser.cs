fd628502e4282733be061b1f9c7b9aac01cbeab8
06needhamt <06needhamt@gmail.com>
2014-09-28 11:17:29 +0100

    More

--- CSharpToNative/CSharpToNative/Parser.cs
+++ CSharpToNative/CSharpToNative/Parser.cs
@@ -18,23 +18,38 @@ namespace CSharpToNative
         {
 
         }
-        public Parser(AST<dynamic,dynamic,dynamic,dynamic> tree)
+        public Parser(AST<dynamic, dynamic, dynamic, dynamic> tree)
         {
             this.tree = tree;
             this.branches = tree.ASTbranches;
+            foreach (ASTBranch<dynamic, dynamic, dynamic, dynamic> branch in branches)
+            {
+                if (IsNumerical(branch.type))
+                {
+                    CreateNumericalInstruction(branch.type, branch.operation, branch.name, branch.Value);
+                }
+                else if (isAlphaNumerical(branch.type))
+                {
+                    CreateAlphaNumericalInstruction(branch.type, branch.operation, branch.name, branch.Value);
+                }
+                else
+                {
+                    CreateBinaryInstruction(branch.type, branch.operation, branch.name, branch.Value);
+                }
+            }
         }
         private void SetBranches()
         {
             this.branches = this.tree.ASTbranches;
         }
-        private List<ASTBranch<dynamic,dynamic,dynamic,dynamic>> GetBranches()
+        private List<ASTBranch<dynamic, dynamic, dynamic, dynamic>> GetBranches()
         {
             return this.branches;
         }
-        private void CreateNumericalInstruction(EnumTypes Type , EnumOperator operation , string name, dynamic val)
+        private void CreateNumericalInstruction(EnumTypes Type, EnumOperator operation, string name, dynamic val)
         {
             List<string> ops = new List<string>(0);
-            DefineVariable(Type,name,null);
+            DefineVariable(Type, name, null);
 
             switch (operation)
             {
@@ -51,7 +66,7 @@ namespace CSharpToNative
                                 //}
                                 for (int j = 0; j < this.tree.ASTbranches.Count; j++)
                                 {
-                                    if (Regex.IsMatch((string) this.tree.ASTbranches.ElementAt<ASTBranch<dynamic,dynamic,dynamic,dynamic>>(j).Value, "([0-9])"))
+                                    if (Regex.IsMatch((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value, "([0-9])"))
                                     {
                                         ops.Add((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value);
                                     }
@@ -71,7 +86,7 @@ namespace CSharpToNative
                                 {
                                     throw new InvalidOperationException("Cannot Create Specified instruction type with no operands");
                                 }
-                                catch(InvalidOperationException ex)
+                                catch (InvalidOperationException ex)
                                 {
                                     Console.ForegroundColor = ConsoleColor.DarkRed;
                                     Console.WriteLine(ex.GetType());
@@ -81,33 +96,225 @@ namespace CSharpToNative
                                 }
                                 finally
                                 {
-                                     Console.ForegroundColor = ConsoleColor.Magenta;
-                                     Console.WriteLine("A FATAL ERROR HAS OCCURED DURING CODE GENERATION : CANNOT CREATE INSTRUCTION WITHOUT AN OPCODE OR OPERANDS ");
-                                     Console.ResetColor();
-                                     System.Threading.Thread.Sleep(2500);
-                                     Environment.Exit(-1);
+                                    Console.ForegroundColor = ConsoleColor.Magenta;
+                                    Console.WriteLine("A FATAL ERROR HAS OCCURED DURING CODE GENERATION : CANNOT CREATE INSTRUCTION WITHOUT AN OPCODE OR OPERANDS ");
+                                    Console.ResetColor();
+                                    System.Threading.Thread.Sleep(2500);
+                                    Environment.Exit(-1);
                                 }
 
                             }
                         }
                         Instruction ins = new Instruction((int)EnumOpcodes.ADD, ops.ToArray<string>());
                         break;
+                        
+                    }
+                    
+                case EnumOperator.UNARY_MINUS:
+                    {
+                        for (int i = 0; i < branches.Count; i++)
+                        {
+                            if (Regex.IsMatch((string)branches.ElementAt(i).Value, "([0-9])"))
+                            {
+                                ops.Add((string)branches.ElementAt(i).Value);
+                                //if(Regex.IsMatch((string)this.tree.getroot(tree).Value,"([0-9])"))
+                                //{
+                                //    ops.Add((string)this.tree.getroot(tree).Value);
+                                //}
+                                for (int j = 0; j < this.tree.ASTbranches.Count; j++)
+                                {
+                                    if (Regex.IsMatch((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value, "([0-9])"))
+                                    {
+                                        ops.Add((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value);
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                            if (ops.Count < 1)
+                            {
+                                try
+                                {
+                                    throw new InvalidOperationException("Cannot Create Specified instruction type with no operands");
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                    Console.WriteLine(ex.GetType());
+                                    Console.WriteLine(ex.Message);
+                                    Console.WriteLine(ex.StackTrace);
+                                    Console.ResetColor();
+                                }
+                                finally
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Magenta;
+                                    Console.WriteLine("A FATAL ERROR HAS OCCURED DURING CODE GENERATION : CANNOT CREATE INSTRUCTION WITHOUT AN OPCODE OR OPERANDS ");
+                                    Console.ResetColor();
+                                    System.Threading.Thread.Sleep(2500);
+                                    Environment.Exit(-1);
+                                }
+
+                            }
+                        }
+                        Instruction ins = new Instruction((int)EnumOpcodes.SUB, ops.ToArray<string>());
+                        break;
+                    }
+                case EnumOperator.UNARY_MULTIPLY:
+                    {
+                        bool issigned = false;
+                        for (int i = 0; i < branches.Count; i++)
+                        {
+                            if (Regex.IsMatch((string)branches.ElementAt(i).Value, "([0-9])"))
+                            {
+                                if ((int)branches.ElementAt(i).Value < 0)
+                                {
+                                    issigned = true;
+                                }
+                                ops.Add((string)branches.ElementAt(i).Value);
+                                //if(Regex.IsMatch((string)this.tree.getroot(tree).Value,"([0-9])"))
+                                //{
+                                //    ops.Add((string)this.tree.getroot(tree).Value);
+                                //}
+                                for (int j = 0; j < this.tree.ASTbranches.Count; j++)
+                                {
+                                    if (Regex.IsMatch((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value, "([0-9])"))
+                                    {
+                                        ops.Add((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value);
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                            if (ops.Count < 1)
+                            {
+                                try
+                                {
+                                    throw new InvalidOperationException("Cannot Create Specified instruction type with no operands");
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                    Console.WriteLine(ex.GetType());
+                                    Console.WriteLine(ex.Message);
+                                    Console.WriteLine(ex.StackTrace);
+                                    Console.ResetColor();
+                                }
+                                finally
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Magenta;
+                                    Console.WriteLine("A FATAL ERROR HAS OCCURED DURING CODE GENERATION : CANNOT CREATE INSTRUCTION WITHOUT AN OPCODE OR OPERANDS ");
+                                    Console.ResetColor();
+                                    System.Threading.Thread.Sleep(2500);
+                                    Environment.Exit(-1);
+                                }
+
+                            }
+                        }
+                        if (issigned)
+                        {
+                            Instruction ins = new Instruction((int)EnumOpcodes.IMUL, ops.ToArray<string>());
+                        }
+                        else
+                        {
+                            Instruction ins = new Instruction((int)EnumOpcodes.MUL,ops.ToArray<string>());
+                        }
+                        
+                        break;
+                    }
+                case EnumOperator.UNARY_DIVIDE:
+                    {
+                        bool issigned = false;
+                        for (int i = 0; i < branches.Count; i++)
+                        {
+                            if (Regex.IsMatch((string)branches.ElementAt(i).Value, "([0-9])"))
+                            {
+                                if ((int)branches.ElementAt(i).Value < 0)
+                                {
+                                    issigned = true;
+                                }
+                                ops.Add((string)branches.ElementAt(i).Value);
+                                //if(Regex.IsMatch((string)this.tree.getroot(tree).Value,"([0-9])"))
+                                //{
+                                //    ops.Add((string)this.tree.getroot(tree).Value);
+                                //}
+                                for (int j = 0; j < this.tree.ASTbranches.Count; j++)
+                                {
+                                    if (Regex.IsMatch((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value, "([0-9])"))
+                                    {
+                                        ops.Add((string)this.tree.ASTbranches.ElementAt<ASTBranch<dynamic, dynamic, dynamic, dynamic>>(j).Value);
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                            if (ops.Count < 1)
+                            {
+                                try
+                                {
+                                    throw new InvalidOperationException("Cannot Create Specified instruction type with no operands");
+                                }
+                                catch (InvalidOperationException ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                    Console.WriteLine(ex.GetType());
+                                    Console.WriteLine(ex.Message);
+                                    Console.WriteLine(ex.StackTrace);
+                                    Console.ResetColor();
+                                }
+                                finally
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Magenta;
+                                    Console.WriteLine("A FATAL ERROR HAS OCCURED DURING CODE GENERATION : CANNOT CREATE INSTRUCTION WITHOUT AN OPCODE OR OPERANDS ");
+                                    Console.ResetColor();
+                                    System.Threading.Thread.Sleep(2500);
+                                    Environment.Exit(-1);
+                                }
+
+                            }
+                        }
+                        if (issigned)
+                        {
+                            Instruction ins = new Instruction((int)EnumOpcodes.IDIV, ops.ToArray<string>());
+                        }
+                        else
+                        {
+                            Instruction ins = new Instruction((int)EnumOpcodes.DIV, ops.ToArray<string>());
+                        }
+                        break;
                     }
             }
-                    return;
-                    // not sure 
+            return;
             
+
         }
 
         private void CreateAlphaNumericalInstruction(EnumTypes Type, EnumOperator operation, string name, dynamic val)
         {
             DefineVariable(Type, name, null);
-           
+
         }
         private void CreateBinaryInstruction(EnumTypes Type, EnumOperator operation, string name, dynamic val)
         {
             DefineVariable(Type, name, null);
-            
+
         }
         private int GetOperation(EnumOperator Operator)
         {
@@ -116,7 +323,7 @@ namespace CSharpToNative
 
         private static dynamic DefineVariable(EnumTypes Type, string name, dynamic val = null)
         {
-           // System.Type var = val.GetType();
+            // System.Type var = val.GetType();
             dynamic memory = val;
             return memory;
         }
@@ -124,7 +331,7 @@ namespace CSharpToNative
         private bool IsNumerical(EnumTypes Type)
         {
             //int[] numericaltypes = { 0, 3, 4, 5, 6, 9, 11 };
-            return (numericaltypes.Contains<int> (Convert.ToInt32(Type)));
+            return (numericaltypes.Contains<int>(Convert.ToInt32(Type)));
         }
 
         private bool isAlphaNumerical(EnumTypes Type)
@@ -135,8 +342,8 @@ namespace CSharpToNative
         {
             return (!alphanumericaltypes.Contains<int>(Convert.ToInt32(Type)) && !numericaltypes.Contains<int>(Convert.ToInt32(Type))) ? true : false;
         }
-         protected bool executebranch(ASTBranch<dynamic,dynamic,dynamic,dynamic> branch)
-         {
+        protected bool executebranch(ASTBranch<dynamic, dynamic, dynamic, dynamic> branch)
+        {
             string varname = branch.name;
             EnumTypes type = branch.type;
             EnumOperator operation = branch.operation;
--- CSharpToNative/CSharpToNative/Program.cs
+++ CSharpToNative/CSharpToNative/Program.cs
@@ -8,9 +8,9 @@ namespace CSharpToNative
 {
     class Program
     {
-        private static List<string> operators = new List<string>(new string[] { "=", "!=", "==", "+", "-", "*", "/", "++#", "#++", "--#", "#--", ">", "<", ">=", "<=", "&&", "&", "||", "|", "!", "~", "^", "+=", "-=", "*=", "/=", "<<", ">>", "%=", "&=", "|=", "^=", "<<=", ">>=", "?:", ".", "," });
-        private static List<string> keywords = new List<string>(new string[] { "public", "protected", "private", "const", "volatile", "unsigned", "unsafe", "new", "continue", "break", "for", "if", "else", "else if", "while", "do", "class", "enum", "interface", "private static", "void" });
-        private static List<string> types = new List<string>(new string[] { "int", "string", "bool", "double", "float", "long", "short", "byte", "char", "decimal", "date", "single", "object" });
+        //private static List<string> operators = new List<string>(new string[] { "=", "!=", "==", "+", "-", "*", "/", "++#", "#++", "--#", "#--", ">", "<", ">=", "<=", "&&", "&", "||", "|", "!", "~", "^", "+=", "-=", "*=", "/=", "<<", ">>", "%=", "&=", "|=", "^=", "<<=", ">>=", "?:", ".", "," });
+        //private static List<string> keywords = new List<string>(new string[] { "public", "protected", "private", "const", "volatile", "unsigned", "unsafe", "new", "continue", "break", "for", "if", "else", "else if", "while", "do", "class", "enum", "interface", "private static", "void" });
+        //private static List<string> types = new List<string>(new string[] { "int", "string", "bool", "double", "float", "long", "short", "byte", "char", "decimal", "date", "single", "object" });
         private static string[] lines;
         private static System.IO.StreamWriter writer;
         private static string currentdir = System.Environment.CurrentDirectory + "/";
@@ -27,8 +27,8 @@ namespace CSharpToNative
             //Environment.Exit(0);
             writer = new System.IO.StreamWriter(currentdir + args[0] + ".lex");
             lines = System.IO.File.ReadAllLines(currentdir + args[0]);
-            Console.WriteLine(operators.Count);
-            Console.WriteLine(keywords.Count);
+            //Console.WriteLine(operators.Count);
+            //Console.WriteLine(keywords.Count);
             //Console.WriteLine(Convert.ToString((EnumOperator)1));
             for (int i = 0; i < lines.Length; i++)
             {
@@ -36,7 +36,7 @@ namespace CSharpToNative
                 //{
                 //    continue;
                 //}
-                if (lines[i].Contains("//"))
+                if (lines[i].StartsWith("//"))
                 {
                     continue;
                 }
@@ -49,8 +49,8 @@ namespace CSharpToNative
             writer.Dispose();
             Console.WriteLine("Lexical Analasis Complete");
             AST<dynamic, dynamic, dynamic, dynamic> tokentree = new AST<dynamic, dynamic, dynamic, dynamic>(Lexer.pubtokens);
-            // encountering error while creating tree
-            Console.WriteLine("Press Any Key To Exit");
+            Parser parse = new Parser(tokentree);
+            Console.WriteLine("Compilation Complete Press Any Key To Exit");
             Console.ReadKey();
         }
 
